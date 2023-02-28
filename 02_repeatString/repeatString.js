6ebbd6703a308f01ee2d0293476ae7e35bd5a49a
kristina <grudyte.kristina@gmail.com>
2023-02-28 11:52:08 +0100

    solve tasks 1 - 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(x, y) {
+    if (y > -1) {
+        return x.repeat(y);
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+    args.forEach((arg) => {
+        let x = array.indexOf(arg);
+        if (x >= 0) {
+            array.splice(x, 1);
+        }
+    })
+    return array;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;

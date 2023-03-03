ff10183a42d4d6d1b5766fe2d981526f103959a3
kristina <grudyte.kristina@gmail.com>
2023-03-03 12:41:44 +0100

    solve sum all task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
 
-};
 
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (typeof x !== 'number' || typeof y !== 'number' || x < 0 || y < 0) {
+        return "ERROR";
+    } else if (x>y) {
+        for (i = x; i > y-1; i--) {
+            sum = sum + i;
+        } 
+        return sum;
+    } else if (y>x) {
+        for (i = x; i < y+1; i++) {
+            sum = sum + i;
+        } 
+        return sum;
+    }
+}
+
+console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
